9c4c1457c9d14e78c3275cb92420189234ba7900
marzaananda <marzaanan21@gmail.com>
2026-05-01 21:57:08 +0700

    tugas:update double linkedlist tugas 2&3

--- tugas2.cpp
+++ tugas2.cpp
@@ -4,10 +4,11 @@ using namespace std;
 struct Node{
 	int data;
 	struct Node *next;
+	struct Node *prev;
 };
 
 void traverse(struct Node *temp); 
-
+void mundur(struct Node *temp);
 int main(){
 	
 	Node *node1 = NULL;
@@ -24,33 +25,53 @@ int main(){
 	
 	node1->data = 10;
 	node1->next = node2;
+	node1->prev = NULL;
 	
 	node2->data = 20;
 	node2->next = node3;
+	node2->prev = node1;
 	
 	node3->data = 30;
 	node3->next = node4;
+	node3->prev = node2;
 	
 	node4->data = 40;
 	node4->next = node5;
+	node4->prev = node3;
 	
 	node5->data = 50;
 	node5->next = NULL;
+	node5->prev = node4;
 
 	cout << "data linked list : " << endl;
 	traverse(node1);
+	mundur(node5);
 	cin.get();
 	return 0;
 }
 
 void traverse(struct Node *temp){
 	int i = 1;
+
 	while (temp != NULL){
 		cout << "Data ke " << i << ": ";
-		cout << temp->data;
+		cout << temp->data<<endl;
+
 		temp = temp->next;
-		cout << endl;
+		
 		i++;
-		cout<<"jumlah datanya ada "<<i;
 	}
+	cout<<"jumlah datanya ada "<<i-1<<endl;
+}
+
+void mundur(struct Node *temp){
+	int i = 1;
+
+	while(temp != NULL){
+		cout << "Data ke " << i << ": " << temp->data << endl;
+		temp = temp->prev;
+		i++;
+	}
+	cout << "jumlah datanya ada " << i-1 << endl;
+
 }
--- tugas3.cpp
+++ tugas3.cpp
@@ -2,59 +2,81 @@
 using namespace std;
 
 struct Node{
-	int data;
-	struct Node *next;
-}a,b;
+    int data;
+    Node *next;
+    Node *prev;
+};
 
-void traverse(struct Node *temp); 
+void traverse(Node *head); 
+void backward(Node *head);
 
 int main(){
-	
-	Node *node1 = NULL;
-	Node *node2 = NULL;
-	Node *node3 = NULL;
-	Node *node4 = NULL;
-	Node *node5 = NULL;
-	
-	node1 = new Node;
-	node2 = new Node;
-	node3 = new Node;
-	node4 = new Node;
-	node5 = new Node;
-	
-	node1->data = 10;
-	node1->next = node2;
-	
-	node2->data = 20;
-	node2->next = node3;
-	
-	node3->data = 30;
-	node3->next = node4;
-	
-	node4->data = 40;
-	node4->next = node5;
-	
-	node5->data = 50;
-	node5->next = NULL;
+    
+    Node *node1 = new Node;
+    Node *node2 = new Node;
+    Node *node3 = new Node;
+    Node *node4 = new Node;
+    Node *node5 = new Node;
+    
+    // circular doubly linked list
+    node1->data = 10;
+    node1->next = node2;
+    node1->prev = node5;
+    
+    node2->data = 20;
+    node2->next = node3;
+    node2->prev = node1;
+    
+    node3->data = 30;
+    node3->next = node4;
+    node3->prev = node2;
+    
+    node4->data = 40;
+    node4->next = node5;
+    node4->prev = node3;
+    
+    node5->data = 50;
+    node5->next = node1;
+    node5->prev = node4;
+    
+    cout << "Forward:\n";
+    traverse(node1);
 
-	cout << "data linked list : " << endl;
-	traverse(node1);
-	cin.get();
-	return 0;
+    cout << "\nBackward:\n";
+    backward(node1);
+
+    return 0;
 }
 
-void traverse(struct Node *temp){
-	int i = 1;
-	int max = temp->data;
-	while (temp != NULL){
-		cout << "Data ke " << i << ": ";
-		cout << temp->data;
-		if(temp->data >max){
-			max=temp->data;
-		}
-		temp = temp->next;
-		cout << endl;
-		i++;
-	}
-	cout<<"data terbesarnya adalah "<<max;
+void traverse(Node *head){
+    Node *temp = head;
+    int i = 1;
+    int max = head->data;
+
+    do{
+        cout << "Data ke " << i << ": " << temp->data << endl;
+        if(temp->data > max){
+            max = temp->data;
+        }
+        temp = temp->next;
+        i++;
+    }while(temp != head);
+
+    cout << "Data terbesar adalah " << max << endl;
+}
+
+void backward(Node *head){
+    Node *temp = head->prev;
+    int i = 1;
+    int max = temp->data;
+
+    do{
+        cout << "Data ke " << i << ": " << temp->data << endl;
+        if(temp->data > max){
+            max = temp->data;
+        }
+        temp = temp->prev;
+        i++;
+    }while(temp != head->prev);
+    cout << "Data terbesar adalah " << max << endl;
 }
